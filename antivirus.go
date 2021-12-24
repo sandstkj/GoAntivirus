a7df9567dc72cc37855b5d391645dba227fdc0a6
Kyle <sandstkj@miamioh.edu>
2021-12-24 17:05:47 -0500

    Create antivirus.go

---
+++ antivirus.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func main() {
+	out, err := exec.Command("ls", "./").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", out)
+}

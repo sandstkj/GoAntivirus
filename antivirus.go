643e80568974a0286a10b8f1790c9b2dfebbcd7d
Kyle <sandstkj@miamioh.edu>
2021-12-24 20:10:18 -0500

    MD5 for Target Directory

--- antivirus.go
+++ antivirus.go
@@ -4,13 +4,34 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 )
 
 func main() {
-	out, err := exec.Command("ls", "./").Output()
+	out, err := exec.Command("ls", "./FilePool").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
+	s := string(out)
+	lines := strings.Split(s, "\n")
+	fmt.Println(lines)
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		input := fmt.Sprintf("./FilePool/%s", line)
+		fmt.Println(input)
+		go func() {
+			out2, err2 := exec.Command("md5", input).Output()
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+			fmt.Println(string(out2))
+		}()
+	}
+	for true {
 
-	fmt.Printf("%s\n", out)
+	}
+
+	fmt.Println(lines)
 }

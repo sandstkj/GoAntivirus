981ee4b1e1329152db2e9a9c480e4e1bacd0bbfa
Kyle <sandstkj@miamioh.edu>
2022-01-03 12:27:48 -0500

    Use WaitGroup for Concurrent Threads

--- antivirus.go
+++ antivirus.go
@@ -5,9 +5,13 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
+
+	// Collect all files in list
 	out, err := exec.Command("ls", "./FilePool").Output()
 	if err != nil {
 		log.Fatal(err)
@@ -15,8 +19,12 @@ func main() {
 	s := string(out)
 	lines := strings.Split(s, "\n")
 	fmt.Println(lines)
+	wg.Add(len(lines))
+	fmt.Println(len(lines))
+
 	for _, line := range lines {
 		if line == "" {
+			wg.Done()
 			continue
 		}
 		input := fmt.Sprintf("./FilePool/%s", line)
@@ -27,11 +35,10 @@ func main() {
 				log.Fatal(err2)
 			}
 			fmt.Println(string(out2))
+			wg.Done()
 		}()
 	}
-	for true {
+	wg.Wait()
 
-	}
-
-	fmt.Println(lines)
+	// fmt.Println(lines)
 }
